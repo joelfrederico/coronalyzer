0f4746f0cb52a31fec71aeafcc8e07108674afe4
Joel Frederico <458871+joelfrederico@users.noreply.github.com>
2020-03-21 17:16:09 -0700

    Update with LA data

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
+#!/usr/bin/env python3
 import os
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objects as go
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+import pandas as pd
+import numpy as np
+import json
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
 server = app.server
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
-])
+la_df = pd.read_json('data/los_angeles.json')
+new_cases_s = la_df['New Cases']
+total_dat = np.empty_like(new_cases_s)
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+total = 0
+for (i, val) in enumerate(new_cases_s):
+    total += val
+    total_dat[i] = total
+
+la_df['Total Cases'] = total_dat
+
+fig = go.Figure(data=[go.Scatter(x=la_df['Date'], y=total_dat)])
+
+app.layout = html.Div([
+    html.H2('Coronalyzer'),
+    dcc.Graph(
+        id='cases',
+        figure=fig
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
